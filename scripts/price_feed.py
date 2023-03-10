6d49006fd7d47456a189a6293845254348aa7eb0
Jazz877 <jazz@kleomed.es>
2023-03-10 09:51:06 +0000

    WIP

--- scripts/price_feed.py
+++ scripts/price_feed.py
@@ -1,8 +1,17 @@
+import click
+import json
+from cosmpy.aerial.config import NetworkConfig
+from loguru import logger
+
+from scripts.client import LedgerClient
+from scripts.utils import get_chain_info, get_network_config_args
+
+
 @click.command()
 @click.option("--lp_addr", prompt="Liquidity Pool address")
 @click.option("--chain_name", prompt="Chain name [ex. juno]")
 def compute_price_feed(lp_addr: str, chain_name: str):
-    logger.info(f"Computing Price Feed for $KLEO {total_rewards} rewards.")
+    logger.info(f"Computing Price Feed for $KLEO on liquidity pool {lp_addr} rewards.")
 
     chain_info = get_chain_info(chain_name)
     network_cfg_kwargs = get_network_config_args(chain_info)
@@ -16,9 +25,14 @@ def compute_price_feed(lp_addr: str, chain_name: str):
 
         encoded_resp = json.loads(resp_cum_prices_wynddao.decode("utf-8"))
 
-        logging.info(f"resp: {encoded_resp}")
+        logger.info(f"resp: {encoded_resp}")
+        val = bytes.fromhex((model["key"])).decode(
+                "utf-8", errors="ignore"
+            )[2:]
+        logger.info(f"value = {}")
     except Exception as e:
         print(f"Error: {e}")
+
 
 if __name__ == "__main__":
     lp_addr = "juno1dpqgt3ja2kdxs94ltjw9ncdsexts9e3dx5qpnl20zvgdguzjelhqstf8zg"
